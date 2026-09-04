a58d2d47aac1c7c9e9bfe582d970fc88df54a84a
Shreyas Sondur <ishfaaq.sheikh@gmail.com>
2026-09-05 02:27:07 +0530

    feat: update app icon, adaptive icons, and dark splash screen assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,19 @@
  * Main navigation layout setup using Expo Router.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { AppProvider } from '@/store/store';
 import colors from '@/constants/colors';
 
+SplashScreen.preventAutoHideAsync().catch(() => {});
+
 export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hideAsync().catch(() => {});
+  }, []);
+
   return (
     <AppProvider>
       <Stack
